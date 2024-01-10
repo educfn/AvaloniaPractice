cd93ad97b0c6d2471dd0954d6b091e0bdd9b6347
Eduardo C F Neto <eduardo@verion.ind.br>
2024-01-10 11:16:57 -0300

    - Adicionado todo o código já presente no projeto "AvaloniaPractice" que foi criado. - Realizando a tarefa "Add Some Layout" do Get Started.

---
+++ AvaloniaPractice/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace AvaloniaPractice.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}

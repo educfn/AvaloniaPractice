ca29d5c126773ea2afe205d3a1a1b6564f9e1ec1
Eduardo C F Neto <eduardo@verion.ind.br>
2024-01-10 16:25:49 -0300

    Code With Controls

--- 1-GetStartedApp/MainWindow.axaml.cs
+++ 1-GetStartedApp/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using System;
 using System.Diagnostics;
 
 namespace GetStartedApp;
@@ -13,6 +14,33 @@ public partial class MainWindow : Window
 
     public void ButtonClicked(object source, RoutedEventArgs args)
     {
-        Debug.WriteLine("Click!");
+        Debug.WriteLine($"Click! Celsius={celsius.Text}");
+
+        if (Double.TryParse(celsius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            fahrenheit.Text = F.ToString("0.0");
+        }
+        else 
+        {
+            celsius.Text = "0";
+            fahrenheit.Text = "0";
+        }
+    }
+
+    public void TextBoxChanged(object source, TextChangedEventArgs args)
+    {
+        Debug.WriteLine($"Click! Celsius={celsius.Text}");
+
+        if (Double.TryParse(celsius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            celsius.Text = "0";
+            fahrenheit.Text = "0";
+        }
     }
 }

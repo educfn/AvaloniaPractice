b62b66bbd6829c8d368499e71ec19808ffe8a4f1
Eduardo C F Neto <eduardo@verion.ind.br>
2024-01-16 15:23:15 -0300

    Create a Model

---
+++ ToDoList/DataModel/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ToDoList.DataModel
+{
+    public class ToDoItem
+    {
+        public string Description { get; set; } = string.Empty;
+        public bool IsChecked { get; set; }
+    }
+}

0f01ea4d6230879cb480ed43c5225ffcb049d5b6
Eduardo C F Neto <eduardo@verion.ind.br>
2024-01-18 13:15:02 -0300

    Navigate Views

--- ToDoList/ViewModels/MainWindowViewModel.cs
+++ ToDoList/ViewModels/MainWindowViewModel.cs
@@ -1,18 +1,32 @@
-﻿using ToDoList.Services;
+﻿using ReactiveUI;
+using ToDoList.Services;
 
 namespace ToDoList.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private ViewModelBase _contentViewModel;
         // this has a dependency on the ToDoListService
 
         public MainWindowViewModel()
         {
             var service = new ToDoListService();
             ToDoList = new ToDoListViewModel(service.GetItems());
+            _contentViewModel = ToDoList;
         }
 
         public ToDoListViewModel ToDoList { get; }
+
+        public ViewModelBase ContentViewModel
+        {
+            get => _contentViewModel;
+            private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+        }
+
+        public void AddItem()
+        {
+            ContentViewModel = new AddItemViewModel();
+        }
     }
 }
 

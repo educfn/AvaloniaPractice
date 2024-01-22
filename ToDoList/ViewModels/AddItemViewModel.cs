a28fd28fec0ccfea28c91a2d84ef9ad1482b68cf
Eduardo C F Neto <eduardo@verion.ind.br>
2024-01-22 13:32:07 -0300

    Add Item Buttons

--- ToDoList/ViewModels/AddItemViewModel.cs
+++ ToDoList/ViewModels/AddItemViewModel.cs
@@ -1,13 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ReactiveUI;
+using System.Reactive;
+using ToDoList.DataModel;
 
 namespace ToDoList.ViewModels
 {
     public class AddItemViewModel : ViewModelBase
     {
-        public string Description { get; set; } = string.Empty;
+        public string _description = string.Empty;
+
+        public ReactiveCommand<Unit, ToDoItem> OkCommand { get; }
+        public ReactiveCommand<Unit, Unit> CancelCommand { get; }
+
+        public AddItemViewModel()
+        {
+            var isValidObservable = this.WhenAnyValue(
+                x => x.Description,
+                x => !string.IsNullOrWhiteSpace(x));
+
+            OkCommand = ReactiveCommand.Create(
+                () => new ToDoItem { Description = Description}, isValidObservable );
+            CancelCommand = ReactiveCommand.Create(() => { });
+        }
+
+        public string Description 
+        {
+            get => this._description;
+            set => this.RaiseAndSetIfChanged(ref _description, value);
+        }
     }
 }

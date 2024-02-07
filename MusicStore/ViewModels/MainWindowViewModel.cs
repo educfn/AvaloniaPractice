38eee4b08dcb29d6b7e6df096af636cfadd82e67
Eduardo C F Neto <eduardo@verion.ind.br>
2024-02-07 11:49:52 -0300

    Button Command

--- MusicStore/ViewModels/MainWindowViewModel.cs
+++ MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,18 @@
-﻿namespace MusicStore.ViewModels
+﻿using ReactiveUI;
+using System.Windows.Input;
+
+namespace MusicStore.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-#pragma warning disable CA1822 // Mark members as static
-        public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+        public ICommand BuyMusicCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            BuyMusicCommand = ReactiveCommand.Create(() =>
+            {
+
+            });
+        }
     }
 }

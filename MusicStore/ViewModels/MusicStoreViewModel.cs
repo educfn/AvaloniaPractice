090e2d18bc53fd9dcade01ece94eb9e579fa22bd
Eduardo C F Neto <eduardo@verion.ind.br>
2024-02-08 10:09:38 -0300

    Mock Search - Checkpoint

--- MusicStore/ViewModels/MusicStoreViewModel.cs
+++ MusicStore/ViewModels/MusicStoreViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ReactiveUI;
 
 namespace MusicStore.ViewModels
 {
-    public class MusicStoreViewModel
+    public class MusicStoreViewModel : ViewModelBase
     {
+        private string? _searchText;
+        private bool _isBusy;
+
+        public string? SearchText
+        {
+            get => _searchText;
+            set => this.RaiseAndSetIfChanged(ref _searchText, value);
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+        }
     }
 }

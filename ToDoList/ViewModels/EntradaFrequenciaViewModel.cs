b1e3a4c27c4a60e11224873969b5f984da7d28ac
Eduardo C F Neto <eduardo@verion.ind.br>
2024-02-09 10:56:24 -0300

    MusicStore Corrigindo o erro no DataType.

---
+++ ToDoList/ViewModels/EntradaFrequenciaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.ViewModels
+{
+    public class EntradaFrequenciaViewModel : ViewModelBase
+    {
+        public EntradaFrequenciaViewModel()
+        {
+        }
+
+
+    }
+}
